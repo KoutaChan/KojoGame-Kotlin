28e1141e5964d55f4547ee562815ef3eb60ee2c2
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-03-19 16:42:30 +0900

    使わないやつを消す

--- main/java/kojogame/koutachan/event/event.kt
+++ main/java/kojogame/koutachan/event/event.kt
@@ -3,7 +3,6 @@ package kojogame.koutachan.event
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.configuration.file.YamlConfiguration
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
