18784ee8d23b394a63b3452370323e8a66666ab5
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-03-25 00:55:10 +0900

    Update ScoreBoard.kt
    眠いので寝る

--- main/java/kojogame/koutachan/model/ScoreBoard.kt
+++ main/java/kojogame/koutachan/model/ScoreBoard.kt
@@ -22,7 +22,11 @@ object scoreboard : Listener {
             obj.setDisplayName("${ChatColor.YELLOW}[ Kojo Game (${Bukkit.getOnlinePlayers().size}) ]")
             obj.getScore("§7${time.year}/${time.month.value}/${time.dayOfMonth} §80.2β").setScore(10) //${time.hour}:${time.minute}:${time.second}
             obj.getScore("").setScore(9)
+            obj.getScore("-----------------------------------").setScore(7)
             GameStateUpdate() //Line 8
+            IronUpdate() //Line 6
+            GoldUpdate() //Line 5
+            DiamondUpdate() //Line 4
     }, 0, 20)
     }
     fun GameStateUpdate() {
@@ -34,6 +38,27 @@ object scoreboard : Listener {
             Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" » ゲーム中").setScore(8)
         } else if (kojogame.koutachan.util.GameState.GameState == 3) {
             Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" » ゲーム終了").setScore(8)
+        }
+    }
+    fun IronUpdate() {
+        if (kojogame.koutachan.util.IronSponges.IronSponges == true) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" スポンジ(1) » §c破壊されています").setScore(6)
+        } else if (kojogame.koutachan.util.IronSponges.IronSponges == false) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" スポンジ(1) » §a破壊されていません").setScore(6)
+        }
+    }
+    fun GoldUpdate() {
+        if (kojogame.koutachan.util.GoldSponges.GoldSponges == true) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" §6スポンジ(2) » §c破壊されています").setScore(5)
+        } else if (kojogame.koutachan.util.GoldSponges.GoldSponges == false) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" §6スポンジ(2) » §a破壊されていません").setScore(5)
+        }
+    }
+    fun DiamondUpdate() {
+        if (kojogame.koutachan.util.DiamondSponges.DiamondSponges == true) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" §bスポンジ(3) » §c破壊されています").setScore(4)
+        } else if (kojogame.koutachan.util.DiamondSponges.DiamondSponges == false) {
+            Bukkit.getScoreboardManager().mainScoreboard.getObjective("KojoGame").getScore(" §bスポンジ(3) » §a破壊されていません").setScore(4)
         }
     }
 }
--- main/java/kojogame/koutachan/util/SpongeState.kt
+++ main/java/kojogame/koutachan/util/SpongeState.kt
@@ -1,4 +1,40 @@
 package kojogame.koutachan.util
 
-class SpongeState {
+class IronSponges(Boolean: Boolean) {
+
+    init {
+        IronSponges = Boolean
+    }
+
+    companion object {
+        var IronSponges: Boolean = false
+        //false = 壊されてない
+        //true = 壊されている
+    }
+}
+
+class GoldSponges(Boolean: Boolean) {
+
+    init {
+        GoldSponges = Boolean
+    }
+
+    companion object {
+        var GoldSponges: Boolean = false
+        //false = 壊されてない
+        //true = 壊されている
+    }
+}
+
+class DiamondSponges(Boolean: Boolean) {
+
+    init {
+        DiamondSponges = Boolean
+    }
+
+    companion object {
+        var DiamondSponges: Boolean = false
+        //false = 壊されてない
+        //true = 壊されている
+    }
 }
